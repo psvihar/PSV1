8700c3816e939ce1d21c119e08d7087e226c0e56
psvihar <77061983+psvihar@users.noreply.github.com>
2021-01-09 01:15:43 +0530

    Delete script.js

--- Publish/js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":958},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-team","displayName":"Team","link":{"linkType":"LinkTypePage","href":"#!page-team"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-info","displayName":"Info","link":{"linkType":"LinkTypePage","href":"#!page-info"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
